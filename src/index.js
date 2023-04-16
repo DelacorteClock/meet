f8a3f1f2c4392170efda5883b2815b7c6f38282b
DelacorteClock <123141973+DelacorteClock@users.noreply.github.com>
2023-04-15 19:50:35 -0700

    Updated for progressive

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import * as atatus from 'atatus-spa';
 atatus.config('6a30d7bd344c4826a7ed4fb49b82b5b0').install();
 
@@ -12,6 +13,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
