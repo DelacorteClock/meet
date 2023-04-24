d55b110e6b4704657b9e76a4753b8d79c86fbcc5
DelacorteClock <123141973+DelacorteClock@users.noreply.github.com>
2023-04-24 10:11:41 -0700

    Update

--- src/EventStyle.js
+++ src/EventStyle.js
@@ -3,21 +3,21 @@ import {PieChart, Pie, Cell, ResponsiveContainer} from 'recharts';
 
 const EventStyle = function ({events}) {
     const [datums, setDatums] = useState([]);
-    const getDatums = function () {
+    const getDatums = () => {
         const styles = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-        const datums = styles.map(function (style) {
+        const keyDatums = styles.map(function (style) {
             const value = events.filter(function (event) {return event.summary.split(' ').includes(style);}).length;
             return {name: style, value: value};
         });
-        return datums;
+        return keyDatums;
     };
-    useEffect(function () {
+    useEffect(() => {
         setDatums(() => {getDatums();});
     }, [events]);
     return (
         <ResponsiveContainer height={350}>
             <PieChart width={300} height={350}>
-                <Pie data={this.getDatums()} cx={200} cy={200} labelLine={false} outerRadius={80} fill='#000000' dataKey='value' label={function ({name, percent}) {return `${name} ${(percent * 100).toFixed(0)}%`;}} ></Pie>
+                <Pie data={datums} cx={200} cy={200} labelLine={false} outerRadius={80} fill='#000000' dataKey='value' label={function ({name, percent}) {return `${name} ${(percent * 100).toFixed(0)}%`;}} ></Pie>
             </PieChart>
         </ResponsiveContainer>
     );
