94f707c291257add317a0d135bae9f09ff7933b7
DelacorteClock <123141973+DelacorteClock@users.noreply.github.com>
2023-04-28 11:47:26 -0700

    Removed overlapping labels and added colour cells

--- src/EventStyle.js
+++ src/EventStyle.js
@@ -3,6 +3,7 @@ import {PieChart, Pie, Cell, ResponsiveContainer} from 'recharts';
 
 const EventStyle = function ({events}) {
     const [datums, setDatums] = useState([]);
+    const colours = ['#00985F', '#CE8E00', '#006EC7', '#C60C30', '#6E267B'];
     const getDatums = () => {
         const styles = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
         const keyDatums = styles.map(function (style) {
@@ -15,10 +16,16 @@ const EventStyle = function ({events}) {
     useEffect(() => {
         setDatums(() => {getDatums();});
     }, [events]);
-    return (
+    return ( 
         <ResponsiveContainer height={350}>
             <PieChart>
-                <Pie data={getDatums()} cx={200} cy={200} labelLine={false} outerRadius={80} fill='#000000' dataKey='value' label={function ({name, percent}) {return `${name} ${(percent * 100).toFixed(0)}%`;}} ></Pie>
+                <Pie data={getDatums()} cx={200} cy={200} labelLine={false} outerRadius={80} dataKey='value' label={function ({name, percent}) {if (percent) {return `${name} ${(percent * 100).toFixed(0)}%`;}}}>
+                    <Cell key={`cell-0`} fill={colours[0]} />
+                    <Cell key={`cell-1`} fill={colours[1]} />
+                    <Cell key={`cell-2`} fill={colours[2]} />
+                    <Cell key={`cell-3`} fill={colours[3]} />
+                    <Cell key={`cell-4`} fill={colours[4]} />
+                </Pie>
             </PieChart>
         </ResponsiveContainer>
     );
